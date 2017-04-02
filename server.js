728733c88a9fe145a79470bd598d39edca80ff46
st3h3n <st3h3n@gmail.com>
2017-04-02 22:53:38 +0200

    deactivated pagecount

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -91,7 +91,7 @@ app.get('/pagecount', function (req, res) {
   } else {
     res.send('{ pageCount: -1 }');
   }
-});
+});*/
 
 // error handling
 app.use(function(err, req, res, next){
